1591dcd6b0c948b16cdacb854c51f7538e9439ba
Jay Hall <jay@alpacalypse.com>
2017-09-11 20:28:12 +1000

    have an error at .post, am commiting a bug :( jonnyB doesn't get a fail, have to figure out in next lesson

--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -8,10 +8,21 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL Constants
+let BASE_URL = "https://chattychatchatchatter.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 //Segues
 let TO_LOGIN = "toLogin"
 
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 
 let UNWIND = "unwindToChannel"
+
+//User DefaultsFoundation
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
 
